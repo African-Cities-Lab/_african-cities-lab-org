f175d68a4142a4aade37296b6e356c42ac1c18a8
nhamidn <hamid-savana@hotmail.fr>
2021-12-09 11:11:47 +0100

    add first test

--- african_cities_lab/home/tests.py
+++ african_cities_lab/home/tests.py
@@ -1,3 +1,14 @@
 # from django.test import TestCase
 
-# Create your tests here.
+from wagtail.tests.utils import WagtailPageTests
+
+from .models import FlatPage
+
+
+def test_imports():
+    from .models import FlatPage  # noqa: F401
+
+
+class FlatPageTests(WagtailPageTests):
+    def test_can_create_a_page(self):
+        self.assertCanCreateAt(FlatPage, FlatPage)
