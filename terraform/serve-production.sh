09433e946e08e0a3dcba7a18caed868b5d53c3e2
Martí Bosch <marti.bosch@protonmail.com>
2021-12-14 14:09:40 +0100

    automate super user creation

--- terraform/serve-production.sh
+++ terraform/serve-production.sh
@@ -1,4 +1,5 @@
 # ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts
 # git clone --single-branch -b staging git@github.com:martibosch/cookiecutter-django-terraform ~/app
 cd ~/app
+docker-compose -f production.yml run --rm django python manage.py migrate
 docker-compose -f production.yml up --build -d
