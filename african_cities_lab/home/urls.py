fce7f82412dfbc31f65234dfa23f0a9930dd634f
Martí Bosch <marti.bosch@protonmail.com>
2022-01-05 16:33:02 +0100

    simplified setup with templateviews

--- african_cities_lab/home/urls.py
+++ african_cities_lab/home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
+    path("", TemplateView.as_view(template_name="home/home.html"), name="home"),
+    path("about/", TemplateView.as_view(template_name="home/about.html"), name="about"),
 ]
--- african_cities_lab/home/views.py
+++ african_cities_lab/home/views.py
@@ -1,10 +1,9 @@
 # from django.shortcuts import render
-from django.views import generic
-
+# from django.views import generic
 
 # Create your views here.
-class HomeView(generic.TemplateView):
-    template_name = "pages/home.html"
+# class HomeView(generic.TemplateView):
+#     template_name = "home/pages/home.html"
 
-    def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         return super().get(request, *args, **kwargs)
