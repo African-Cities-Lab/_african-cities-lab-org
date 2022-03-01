3a2ae2397ffb4a6fe4d5edb297b7cd0010f00e3b
nhamidn <nhamid@student.1337.ma>
2022-03-01 22:21:59 +0000

    add latest news to home page

--- african_cities_lab/home/urls.py
+++ african_cities_lab/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home/home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name="home/home.html"), name="home"),
     # path("about/", TemplateView.as_view(template_name="home/about.html"), name="about"),
     path(
         "courses/",
