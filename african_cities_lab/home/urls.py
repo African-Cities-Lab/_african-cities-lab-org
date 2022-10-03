6175956040d77fc08eca14189c9b977e3693ce3f
Martí Bosch <marti.bosch@protonmail.com>
2022-10-03 17:44:26 +0200

    fix: DRY subscription, catch subscribed mail error

--- african_cities_lab/home/urls.py
+++ african_cities_lab/home/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 from . import views
 
-urlpatterns = [ 
+urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("about/", TemplateView.as_view(template_name="home/about.html"), name="about"),
     path(
@@ -31,21 +31,22 @@ urlpatterns = [
         TemplateView.as_view(template_name="home/moocs.html"),
         name="moocs",
     ),
-    path("integration-of-open-data-and-artificial-intelligence-in-the-development-of-smart-cities-in-Africa/", 
-         TemplateView.as_view(template_name="home/events/1st_webinar.html"), 
-         name="integration-of-open-data-and-artificial-intelligence-in-the-development-of-smart-cities-in-Africa"
-    ),
-    path("african-cities-what-role-for-the-youth/", 
-         TemplateView.as_view(template_name="home/events/2nd_webinar.html"), 
-         name="african-cities-what-role-for-the-youth"
+    path(
+        "integration-of-open-data-and-artificial-intelligence-in-the-development-of-smart-cities-in-Africa/",
+        TemplateView.as_view(template_name="home/events/1st_webinar.html"),
+        name="integration-of-open-data-and-artificial-intelligence-in-the-development-of-smart-cities-in-Africa",
     ),
     path(
-        "webinar-registration/", 
-         TemplateView.as_view(template_name="home/webinar_form.html"), 
-         name="webinar-registration"
+        "african-cities-what-role-for-the-youth/",
+        TemplateView.as_view(template_name="home/events/2nd_webinar.html"),
+        name="african-cities-what-role-for-the-youth",
     ),
-    path("webinar-subscribe/", views.WebinarSubscription.subscribe, name="subscribe"),
-    
+    path(
+        "webinar-registration/",
+        TemplateView.as_view(template_name="home/webinar_form.html"),
+        name="webinar-registration",
+    ),
+    path("webinar-subscribe/", views.subscribe_webinar, name="subscribe-webinar"),
     path(
         "the-international-conference-of-african-cities-2023/",
         TemplateView.as_view(template_name="home/events/civa/civa.html"),
@@ -71,56 +72,75 @@ urlpatterns = [
         TemplateView.as_view(template_name="home/events/civa/travel_info.html"),
         name="civa-travel-information",
     ),
-    
     # Moocs url
     path(
         "moocs/introduction-to-sanitazation-economy-systems-markets/",
-        TemplateView.as_view(template_name="home/moocs/introduction_to_sanitazation_economy_systems_markets.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/introduction_to_sanitazation_economy_systems_markets.html"
+        ),
         name="introduction-to-sanitazation-economy-systems-markets",
     ),
     path(
         "moocs/urban-data-management-for-smart-city-developpement-in-africa/",
-        TemplateView.as_view(template_name="home/moocs/urban_data_management_for_smart_city_developpement_in_africa.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/urban_data_management_for_smart_city_developpement_in_africa.html"
+        ),
         name="urban-data-management-for-smart-city-developpement-in-africa",
     ),
     path(
         "moocs/eco-construction-en-afrique/",
-        TemplateView.as_view(template_name="home/moocs/eco_construction_en_afrique.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/eco_construction_en_afrique.html"
+        ),
         name="eco-construction-en-afrique",
     ),
     path(
         "moocs/making-housing-work-in-african-cities/",
-        TemplateView.as_view(template_name="home/moocs/making_housing_work_in_african_cities.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/making_housing_work_in_african_cities.html"
+        ),
         name="making-housing-work-in-african-cities",
     ),
     path(
         "moocs/how-can-cities-in-africa-be-smarter/",
-        TemplateView.as_view(template_name="home/moocs/how_can_cities_in_africa_be_smarter.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/how_can_cities_in_africa_be_smarter.html"
+        ),
         name="how-can-cities-in-africa-be-smarter",
     ),
     path(
         "moocs/what-can-food-tell-us-about-cities-in-africa/",
-        TemplateView.as_view(template_name="home/moocs/what_can_food_tell_us_about_cities_in_africa.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/what_can_food_tell_us_about_cities_in_africa.html"
+        ),
         name="what-can-food-tell-us-about-cities-in-africa",
     ),
     path(
         "moocs/la-fabrique-urbaine-collective/",
-        TemplateView.as_view(template_name="home/moocs/la_fabrique_urbaine_collective.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/la_fabrique_urbaine_collective.html"
+        ),
         name="la-fabrique-urbaine-collective",
     ),
     path(
         "moocs/urban-economic-development-in-the-context-of-climate-change/",
-        TemplateView.as_view(template_name="home/moocs/urban_economic_development_in_the_context_of_climate_change.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/urban_economic_development_in_the_context_of_climate_change.html"
+        ),
         name="urban-economic-development-in-the-context-of-climate-change",
     ),
     path(
         "moocs/digital-governance-for-a-better-public-participation-and-urban-services-delivery-in-african-cities/",
-        TemplateView.as_view(template_name="home/moocs/digital_governance_for_a_better_public_participation_and_urban_services_delivery_in_african_cities.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/digital_governance_for_a_better_public_participation_and_urban_services_delivery_in_african_cities.html"
+        ),
         name="digital-governance-for-a-better-public-participation-and-urban-services-delivery-in-african-cities",
     ),
     path(
         "moocs/le-solaire-photovoltaïque-et-ses-applications-au-profit-du-developpement-urbain/",
-        TemplateView.as_view(template_name="home/moocs/le_solaire_photovoltaïque_et_ses_applications_au_profit_du_developpement_urbain.html"),
+        TemplateView.as_view(
+            template_name="home/moocs/le_solaire_photovoltaïque_et_ses_applications_au_profit_du_developpement_urbain.html"
+        ),
         name="le-solaire-photovoltaïque-et-ses-applications-au-profit-du-developpement-urbain",
     ),
     path(
@@ -138,6 +158,5 @@ urlpatterns = [
         TemplateView.as_view(template_name="home/moocs/urban_ai.html"),
         name="urban-ai",
     ),
-    path("moocs/registered/", views.moocs_registered, name="moocs-registered"),
-    
+    path("moocs/subscribe/", views.subscribe_moocs, name="subscribe-moocs"),
 ]
--- african_cities_lab/home/views.py
+++ african_cities_lab/home/views.py
@@ -1,17 +1,18 @@
 # from django.shortcuts import render
 # from multiprocessing import context
-
-from django.http import HttpResponse
+import json
 from unicodedata import category
+
 from django.apps import apps
 from django.conf import settings
 from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.datastructures import MultiValueDictKeyError
 from django.utils.translation import gettext_lazy as _
 from django.views import generic
 from mailchimp_marketing import Client
 from mailchimp_marketing.api_client import ApiClientError
-from django.utils.datastructures import MultiValueDictKeyError
 from wagtail.core.models import Locale
 
 
@@ -36,13 +37,14 @@ class HomeView(generic.TemplateView):
         )
         context["blog"] = latest_article
         return context
-    
-    
-def _moocs_registered(email, merge_fields=None):
+
+
+def _subscribe(email, list_id, merge_fields=None):
     """
     Contains code handling the communication to the mailchimp api
     to create a contact/member in an audience/list.
     """
+
     mailchimp = Client()
     mailchimp.set_config(
         {
@@ -60,124 +62,74 @@ def _moocs_registered(email, merge_fields=None):
         member_info["merge_fields"] = merge_fields
 
     try:
-        response = mailchimp.lists.add_list_member(
-            settings.MAILCHIMP_MOOCS_LIST_ID, member_info
-        )
-        print("response: {}".format(response))
+        response = mailchimp.lists.add_list_member(list_id, member_info)
+        print(f"API call successful: {response}")
+        return response.status
     except ApiClientError as error:
-        print("An exception occurred: {}".format(error.text))
-        
-    
-def moocs_registered(request):
+        print(f"An exception occurred: {error.text}")
+        if json.loads(error.text)["title"] == "Member Exists":
+            return "exists"
+
+
+def subscribe_moocs(request):
     if request.method == "POST":
         email = request.POST["EMAIL"]
         merge_fields = {
             "NAME": request.POST["NAME"],
         }
-        
-        _moocs_registered(email, merge_fields)
-        messages.success(
-            request, _("Thank you! Your registration has been successful.")
-        )  # message
-             
+
+        status = _subscribe(email, settings.MAILCHIMP_MOOCS_LIST_ID, merge_fields)
+        if status == "subscribed":
+            messages.success(
+                request, _("Thank you! Your registration has been successful.")
+            )  # message
+        elif status == "exists":
+            messages.info(
+                request,
+                _(
+                    "Your email is already registered, please check your mailbox or your spam. Thank you!"
+                ),
+            )  # message
+
+    return render(request, "home/home.html")
+
+
+def subscribe_webinar(request):
+    if request.method == "POST":
+        email = request.POST["EMAIL"]
+        merge_fields = {
+            "LNAME": request.POST["LNAME"],
+            "FNAME": request.POST["FNAME"],
+            "INSTITUT": request.POST["INSTITUT"],
+            "FUNCTION": request.POST["FUNCTION"],
+            "TITLE": request.POST["TITLE"],
+            "COUNTRY": request.POST["COUNTRY"],
+            "LINKEDIN": request.POST["LINKEDIN"],
+        }
+
+        if request.POST["site_language"] == "en":
+            list_id = settings.MAILCHIMP_WEBINAR_EN_LIST_ID
+        else:  # "fr"
+            list_id = settings.MAILCHIMP_WEBINAR_FR_LIST_ID
+
+        status = _subscribe(email, list_id, merge_fields)
+        if status == "subscribed":
+            messages.success(
+                request,
+                _(
+                    "Thank you for registering! A confirmation email has been sent to you, please check your mailbox or your spam."
+                ),
+            )  # message
+        elif status == "exists":
+            messages.info(
+                request,
+                _(
+                    "Your email is already registered, please check your mailbox or your spam. Thank you!"
+                ),
+            )  # message
+
     return render(
-            request,
-            "home/home.html"
-        )
-  
-
-def _webinar_subscription_en(email, merge_fields=None):
-    """
-    Contains code handling the communication to the mailchimp api
-    to create a contact/member in an audience/list.
-    """
-
-    mailchimp = Client()
-    mailchimp.set_config(
-        {
-            "api_key": settings.MAILCHIMP_API_KEY,
-            "server": settings.MAILCHIMP_DATA_CENTER,
-        }
+        request,
+        "home/webinar_form.html",
+        context={"title": _("Subscribe to the webinar")},
     )
-
-    member_info = {
-        "email_address": email,
-        "status": "subscribed",
-    }
-
-    if merge_fields is not None:
-        member_info["merge_fields"] = merge_fields
-
-    try:
-        response = mailchimp.lists.add_list_member(
-            settings.MAILCHIMP_WEBINAR_EN_LIST_ID, member_info
-        )
-        print("response: {}".format(response))
-    except ApiClientError as error:
-        print("An exception occurred: {}".format(error.text))
-        
-        
-def _webinar_subscription_fr(email, merge_fields=None):
-    """
-    Contains code handling the communication to the mailchimp api
-    to create a contact/member in an audience/list.
-    """
-
-    mailchimp = Client()
-    mailchimp.set_config(
-        {
-            "api_key": settings.MAILCHIMP_API_KEY,
-            "server": settings.MAILCHIMP_DATA_CENTER,
-        }
-    )
-
-    member_info = {
-        "email_address": email,
-        "status": "subscribed",
-    }
-
-    if merge_fields is not None:
-        member_info["merge_fields"] = merge_fields
-
-    try:
-        response = mailchimp.lists.add_list_member(
-            settings.MAILCHIMP_WEBINAR_FR_LIST_ID, member_info
-        )
-        print(f'API call successful: {response.status}')
-    except ApiClientError as error:
-        print(f'An exception occurred: {error.text}')     
-        
-class WebinarSubscription(generic.TemplateView):
-    
-    def subscribe(request):
-        if request.method == "POST":
-            email = request.POST["EMAIL"]
-            merge_fields = {
-                "LNAME": request.POST["LNAME"],
-                "FNAME": request.POST["FNAME"],
-                "INSTITUT": request.POST["INSTITUT"],
-                "FUNCTION": request.POST["FUNCTION"],
-                "TITLE": request.POST["TITLE"],
-                "COUNTRY": request.POST["COUNTRY"],
-                "LINKEDIN": request.POST["LINKEDIN"],
-            }
-            
-            if request.POST["site_language"] == "en":
-                _webinar_subscription_en(email, merge_fields)  # function to access mailchimp
-                messages.success(
-                    request, _("Thank you for registering! A confirmation email has been sent to you, please check your mailbox or your spam.")
-                )  # message
-                
-            if request.POST["site_language"] == "fr":
-                _webinar_subscription_fr(email, merge_fields)  # function to access mailchimp
-                messages.success(
-                    request, _("Merci de votre inscription ! Un mail de confirmation vous a été envoyé, veuillez consulter votre boîte mail ou vos spams.")
-                )  # message
-
-        return render(
-            request,
-            "home/webinar_form.html",
-            context={"title": _("Subscribe to the webinar")},
-        )
-
-
